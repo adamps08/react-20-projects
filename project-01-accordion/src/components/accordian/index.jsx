b3f86b2538770000d153944804207ddd812325fc
Adam Smith <adamps724@gmail.com>
2025-06-04 11:59:58 -0500

    single selection accordion complete

--- project-01-accordion/src/components/accordian/index.jsx
+++ project-01-accordion/src/components/accordian/index.jsx
@@ -1,26 +1,31 @@
 // single selection
-// multiple selection
 
 import { useState } from "react"
 import data from "./data";
+import "./styles.css";
 export default function Accordian() {
     const [selected, setSelected] = useState(null);
 
+  function handleSingleSelection(getCurrentId){
+    setSelected(getCurrentId === selected ? null : getCurrentId)
+  } 
+  
+  console.log(selected);
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem, index) => (
             <div className="item" key={index}>
-              <div className="title" onClick={() => setSelected(index)}>
+              <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                 <h3>{dataItem.question}</h3>
-                <span>{selected === index ? "-" : "+"}</span>
+                <span>+</span>
               </div>
-              {selected === index && (
-                <div className="content">
-                  <p>{dataItem.answer}</p>
-                </div>
-              )}
+              {
+                selected === dataItem.id ? 
+                <div className= "content">{dataItem.answer}</div>
+                : null
+              }
             </div>
           ))
         ) : (
@@ -30,3 +35,5 @@ export default function Accordian() {
     </div>
   );
 }
+
+// multiple selection
