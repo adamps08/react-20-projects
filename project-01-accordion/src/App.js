1146b84566ba547dd5d4ea8414624ead2f64f1be
Adam Smith <adamps724@gmail.com>
2025-06-05 19:01:17 -0500

    begun image-slider

--- project-01-accordion/src/App.js
+++ project-01-accordion/src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import Accordian from './components/accordian';
-import RandomColor from './components/accordian/random-color';
+import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
     <div className="App">
       {/*Accordion Component */}
-      <Accordian/>
-
-       {/*Random Color Component */}
-      <RandomColor/>
+      <Accordian />
+      {/*Random Color Component */}
+      <RandomColor />
       {/* Star Rating Component */}
-      <StarRating noOfStars={10}/>
+      <StarRating noOfStars={10} />
+      {/* Image Slider Component */}
+      <ImageSlider 
+        url={"https://picsum.photos/v2/list"} 
+        page= {'1'} 
+        limit={'10'}
+      />
     </div>
   );
 }
