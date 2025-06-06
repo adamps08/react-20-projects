653a3cf76b4fd042c3d91161248eceee038e7203
Adam Smith <adamps724@gmail.com>
2025-06-06 16:37:53 -0500

    begun load more data componenet

--- project-01-accordion/src/App.js
+++ project-01-accordion/src/App.js
@@ -3,6 +3,7 @@ import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
         page= {'1'} 
         limit={'10'}
       />
+      {/* Load More PRoducts Component */}
+      <LoadMoreData/>
     </div>
   );
 }
